cbd7d5befc83868c2d389ff109f979b6aa775e0d
virudwivedi <virendrakumardwivedi10@gmail.com>
2022-03-23 19:46:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>
+         <h1>WELCOME </h1>
+         <h2>!HELLO WORLD!</h2>
+       </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
